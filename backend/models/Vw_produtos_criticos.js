b211e5696c047968982269492922ba5f49fa828a
ELIEL BERNARDI <eliel_bernardi1@estudante.sesisenai.org.br>
2026-08-03 09:35:52 -0300

    refactor(models): formatar models de view de produtos criticos e volume de compras

--- backend/models/Vw_produtos_criticos.js
+++ backend/models/Vw_produtos_criticos.js
@@ -1,23 +1,23 @@
-const { DataTypes } = require('sequelize');
-const db = require('../db/conn');
+const { DataTypes } = require('sequelize')
+const db = require('../db/conn')
 
-const VwProdutosCriticos = db.define('VwProdutosCriticos', {
-  codigo_produto: {
-    type: DataTypes.INTEGER,
-    primaryKey: true
-  },
-  nome: {
-    type: DataTypes.STRING
-  },
-  categoria: {
-    type: DataTypes.STRING
-  },
-  quantidade_atual: {
-    type: DataTypes.INTEGER
-  }
-}, {
-  timestamps: false,
-  tableName: 'vw_produtos_criticos'
-});
+const VwProdutosCriticos = db.define('VwProdutosCriticos',{
+    codigo_produto: {
+        type: DataTypes.INTEGER,
+        primaryKey: true
+    },
+    nome: {
+        type: DataTypes.STRING(100),
+    },
+    categoria: {
+        type: DataTypes.STRING(100),
+    },
+    quantidade_atual: {
+        type: DataTypes.INTEGER,
+    }
+},{
+    timestamps: false,
+    tableName: 'vw_produtos_criticos'
+})
 
-module.exports = VwProdutosCriticos;
+module.exports = VwProdutosCriticos
--- backend/models/Vw_volume_compras.js
+++ backend/models/Vw_volume_compras.js
@@ -1,20 +1,20 @@
-const { DataTypes } = require('sequelize');
-const db = require('../db/conn');
+const { DataTypes } = require('sequelize')
+const db = require('../db/conn')
 
-const VwVolumeCompras = db.define('VwVolumeCompras', {
-  nome: {
-    type: DataTypes.STRING,
-    primaryKey: true
-  },
-  quantidade_total_movimentada: {
-    type: DataTypes.INTEGER
-  },
-  valor_financeiro_movimentado: {
-    type: DataTypes.DECIMAL(10, 2)
-  }
-}, {
-  timestamps: false,
-  tableName: 'vw_volume_compras'
-});
+const VwVolumeCompras = db.define('VwVolumeCompras',{
+    nome: {
+        type: DataTypes.STRING(100),
+        primaryKey: true
+    },
+    quantidade_total_movimentada: {
+        type: DataTypes.INTEGER,
+    },
+    valor_financeiro_movimentado: {
+        type: DataTypes.DECIMAL(10,2),
+    }
+},{
+    timestamps: false,
+    tableName: 'vw_volume_compras'
+})
 
-module.exports = VwVolumeCompras;
+module.exports = VwVolumeCompras
