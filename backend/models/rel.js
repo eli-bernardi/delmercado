f96501878aaab59c4e8748bc9f34176aa0bac8c8
ELIEL BERNARDI <eliel_bernardi1@estudante.sesisenai.org.br>
2026-08-03 09:35:44 -0300

    refactor(models): formatar associacoes de relacao no estilo do professor

--- backend/models/rel.js
+++ backend/models/rel.js
@@ -2,28 +2,28 @@ const Usuario = require('./Usuario')
 const Produto = require('./Produto')
 const Compra = require('./Compra')
 
-Usuario.hasMany(Compra, {
-      foreignKey: 'codUsuario',
-      as: 'compras',
-      onDelete: 'CASCADE'
+Usuario.hasMany(Compra,{
+    foreignKey: 'codUsuario',
+    as: 'compras',
+    onDelete: 'CASCADE'
 })
 
-Compra.belongsTo(Usuario, {
-      foreignKey: 'codUsuario',
-      as: 'usuario',
-      allowNull: false
+Compra.belongsTo(Usuario,{
+    foreignKey: 'codUsuario',
+    as: 'usuario',
+    allowNull: false
 })
 
-Produto.hasMany(Compra, {
-      foreignKey: 'codProduto',
-      as: 'compras',
-      onDelete: 'CASCADE'
+Produto.hasMany(Compra,{
+    foreignKey: 'codProduto',
+    as: 'compras',
+    onDelete: 'CASCADE'
 })
 
-Compra.belongsTo(Produto, {
-      foreignKey: 'codProduto',
-      as: 'produto',
-      allowNull: false
+Compra.belongsTo(Produto,{
+    foreignKey: 'codProduto',
+    as: 'produto',
+    allowNull: false
 })
 
 module.exports = { Usuario, Produto, Compra }
